f536c8c2d6057882b0e841e978c75fe343a1f01b
pederba <peder.basmo@gmail.com>
2023-11-22 09:11:58 +0000

    environment setup done

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/robot_learning_ws/src
+# Build directory: /root/robot_learning_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ex1_pkg")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/root/robot_learning_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/root/robot_learning_ws/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/root/robot_learning_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/root/robot_learning_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/root/robot_learning_ws/src:$ROS_PACKAGE_PATH"
